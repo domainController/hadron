ae6aab1287ba0d5b8bd85c6816b0a5758bd67905
kevin-garnette <noyau.imperatif@gmail.com>
2020-03-05 12:43:03 +0000

    express, postman, a couple of steps

--- expresscrasscourse/index.js
+++ expresscrasscourse/index.js
@@ -2,7 +2,14 @@
 // https://www.youtube.com/watch?v=L72fhGm1tfE&t=743s
 
 const express = require('express');
+const path = require('path');
+const micronutriments = require('./micronutriments.js')
 const app = express();
-app.listen();
-const PORT = process.env.PORT || 5000;
-app.listen(PORT), () => console.log(`Server started on port ${PORT}`));
+
+
+app.get('/api/micronutriments', (req, res) => {
+	res.json(micronutriments);
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
