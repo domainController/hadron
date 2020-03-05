82c5db4fa71c4c7f500292fe229e9e1a1fad2ae9
kevin-garnette <noyau.imperatif@gmail.com>
2020-03-05 19:17:01 +0000

    have build my first API

--- expresscrasscourse/index.js
+++ expresscrasscourse/index.js
@@ -3,12 +3,31 @@
 
 const express = require('express');
 const path = require('path');
-const micronutriments = require('./micronutriments.js')
+
+const micronutrients = require('./micronutrients.js');
 const app = express();
 
+const logger = require('./middleware/logger.js');
 
-app.get('/api/micronutriments', (req, res) => {
-	res.json(micronutriments);
+// Init middle ware
+// app.use(logger);
+
+// Get All micronutrients
+app.get('/api/micronutrients', (req, res) => {
+	res.json(micronutrients);
+});
+
+// Get single micronutrient
+app.get('/api/micronutrients/:price', (req, res) => {
+	
+	const found = micronutrients.some(micronutrient => micronutrient.price === parseInt(req.params.price));
+
+	if(found){
+		res.json(micronutrients.filter(micronutrient => micronutrient.price === parseInt(req.params.price)));
+	}
+	else {
+		res.status(400).json({ msg: `No micronutrient with price ${req.params.price}`});
+	}
 });
 
 const PORT = process.env.PORT || 8000;
---
+++ expresscrasscourse/routes/api/micronutrientsRoutes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const micronutrients = require(./'micronutrients.js');
+
+// Get All micronutrients:
+
+router.get('/api/micronutrients', (req, res) => {
+	res.json(micronutrients);
+});
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Get single micronutrient:
+
+router.get('/api/micronutrients/:price', (req, res) => {
+	
+	const found = micronutrients.some(micronutrient => micronutrient.price === parseInt(req.params.price));
+
+	if(found){
+		res.json(micronutrients.filter(micronutrient => micronutrient.price === parseInt(req.params.price)));
+	}
+	else {
+		res.status(400).json({ msg: `No micronutrient with price ${req.params.price}`});
+	}
+});
+
+
+// Create a micronutrient
+
+router.post('/api/micronutrients', (req, res) => {
+	res.send(req.body):
+});
+module.exports = router;
