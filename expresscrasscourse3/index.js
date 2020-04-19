a3c40a6065be1964a55f0194358c606b7afa9b2a
kevin-garnette <noyau.imperatif@gmail.com>
2020-04-19 17:26:56 +0100

    express crash course up to 33 min

--- expresscrasscourse3/index.js
+++ expresscrasscourse3/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
-
+const logger = require('./middleware/logger');
+const micronutrients = require('./Micronutrients');
 const app = express();
 
-app.get('/api/micronutriments');
+// Get single Member
+app.get('/api/micronutrients/:description', (req, res) => {
+    res.json(micronutrients.filter(micronutrients => micronutrient.description === req.params.description));
+});
 
+// Init middleware
+app.use(logger);
+
+// Get all micronutrients
+app.get('/api/micronutrients', (req, res) => res.json(micronutrients));
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')))
