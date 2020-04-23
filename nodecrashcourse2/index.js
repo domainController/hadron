5458ba636e2de9c0d555007f54371ffdd0d73326
kevin-garnette <noyau.imperatif@gmail.com>
2020-04-23 13:23:15 +0100

    end of node crash course w/out heroku

--- nodecrashcourse2/index.js
+++ nodecrashcourse2/index.js
@@ -4,10 +4,66 @@ const fs = require('fs');
 const micronutrients = require('./micronutrients');
 
 const server = http.createServer((req, res)=> {
-    if (req.url === '/api/micronutrients') {
+/*     if (req.url === '/api/micronutrients') {
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(micronutrients));
-        }});
+        }}); */
+
+// Build file path
+let filePath = path.join(
+    __dirname,
+     'public',
+      req.url === '/formCategories.html' ? 'formCategories.html' : req.url
+);
+
+// Extension of the file
+let extname = path.extname(filePath);
+
+// Initial content type
+let contentType ='text/html';
+
+// Check ext and set content type
+
+switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  // Read File
+  fs.readFile(filePath, (err,content) => {
+    if(err) {
+      if(err.code == 'ENOENT') {
+          // Page not found
+          fs.readFile(path.join(__dirname, 'public', '404.html'),
+          ( err, content) => {
+              res.writeHead(200, {'Content-Type':'text/html'});
+              res.end(content, 'utf-8');
+          })
+        } else {
+            // some server error
+            res.writeHead(500);
+            res.end(`Server Error: ${err.code}`);
+        }
+      } else {
+            // Success
+          res.writeHead(200, {'Content-Type': contentType });
+          res.end(content, 'utf-8');
+      }
+    });
+});
 
 const PORT = process.env.PORT || 5000 ;
 
