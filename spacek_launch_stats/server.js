4b9e1e8d0aca619153357e9c312bb3ca836ab697
kevin-garnette <noyau.imperatif@gmail.com>
2020-05-10 14:43:29 +0100

    graphql is fucking cool man !

--- spacek_launch_stats/server.js
+++ spacek_launch_stats/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const grapqlHTTP = require('express-graphql');
+const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 
 const app = express();
 
 app.use(
   '/graphql',
-  grapqlHTTP({
+  graphqlHTTP({
     schema,
     graphiql: true,
   })
