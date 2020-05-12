83e26039d9c4357d05ec3b60bc237a7ed0f2cefb
kevin-garnette <noyau.imperatif@gmail.com>
2020-05-12 12:10:38 +0100

    added OKC JSON profiles for GraphQL tutorial

--- customerbase/schema.js
+++ customerbase/schema.js
@@ -10,20 +10,20 @@ const {
 } = require('graphql');
 
 // Hardcoded data
-const customers = [
-  { id: '1', name: 'Penny Hardaway', email: 'hundred.ml@gmail.com', age: 47 },
-  { id: '2', name: 'Nick Van Excel', email: 'thousand.ml@gmail.com', age: 44 },
-  { id: '3', name: 'Harold Miner', email: 'millions.ml@gmail.com', age: 45 },
+const micronutrients = [
+  { id: '1', name: 'Multivitamins', site: 'iHerb', price: 28 },
+  { id: '2', name: 'Magnesium', site: 'amazon.co.uk', price: 12 },
+  { id: '3', name: 'Potassium', site: 'amazon.co.uk', price: 9 },
 ];
 
-// Customer Type
-const CustomerType = new GraphQLObjectType({
-  name: 'Customer',
+// Micronutrient Type
+const MicronutrientType = new GraphQLObjectType({
+  name: 'Micronutrient',
   fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    email: { type: GraphQLString },
-    age: { type: GraphQLInt },
+    site: { type: GraphQLString },
+    price: { type: GraphQLInt },
   }),
 });
 
@@ -31,23 +31,23 @@ const CustomerType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    customer: {
-      type: CustomerType,
+    Micronutrients: {
+      type: MicronutrientType,
       args: {
         id: { type: GraphQLString },
       },
       resolve(parentValue, args) {
-        for (let i = 0; i < customers.length; i++) {
-          if (customers[i].id == args.id) {
-            return customers[i];
+        for (let i = 0; i < micronutrients.length; i++) {
+          if (micronutrients[i].id == args.id) {
+            return micronutrients[i];
           }
         }
       },
     },
-    customers: {
-      type: new GraphQLList(CustomerType),
+    micronutrients: {
+      type: new GraphQLList(MicronutrientType),
       resolve(parentValue, args) {
-        return customers;
+        return micronutrients;
       },
     },
   },
