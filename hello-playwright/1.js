e9c6c66a3ce5eee6008bfda323ac5100aafe5218
kevin-garnette <noyau.imperatif@gmail.com>
2020-05-22 09:47:58 +0100

    moves towards new automate & scrap oriented repo

--- hello-playwright/1.js
+++
@@ -1,9 +0,0 @@
-const { firefox } = require('playwright');
-
-(async () => {
-  const browser = await firefox.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.okcupid.com/');
-  await page.screenshot({ path: `okCupidLandingPage.png` });
-  await browser.close();
-})();
--- hello-playwright/FirstScript.js
+++
@@ -1,9 +0,0 @@
-const { webkit } = require('playwright');
-
-(async () => {
-  const browser = await webkit.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.okcupid.com/');
-  await page.screenshot({ path: `example.png` });
-  await browser.close();
-})();
