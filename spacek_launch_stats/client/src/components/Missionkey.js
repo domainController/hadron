83dd62ed6938c40dc392e86944e6fb81b903c9bf
kevin-garnette <noyau.imperatif@gmail.com>
2020-05-24 14:11:32 +0100

    GraphQL With React & Apollo [3] - Finishing The App up to 15 mn

---
+++ spacek_launch_stats/client/src/components/Missionkey.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Missionkey() {
+  return (
+    <div className='my-3'>
+      <p>
+        <span class='px-3 mr-2 bg-success' /> =Success
+      </p>
+      <p>
+        <span class='px-3 mr-2 bg-danger' /> =Failed
+      </p>
+    </div>
+  );
+}
